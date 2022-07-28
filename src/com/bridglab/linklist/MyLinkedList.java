e80f29b10fc8f61321bc89d4b7971127372f0aab
tejuypatil <tejuypatil@gmail.com>
2022-07-28 14:53:50 +0530

    Added Program to create linked List

--- src/com/bridglab/linklist/MyLinkedList.java
+++ src/com/bridglab/linklist/MyLinkedList.java
@@ -1,35 +1,14 @@
 package com.bridglab.linklist;
 
+
+
 public class MyLinkedList {
-    public INode tail;
-    public INode head;
-    public MyLinkedList()
-    {
-        this.head=null;
-        this.tail=null;
-        }
-public void add(INode newNode){
-        if(this.tail==null){
-        this.tail=newNode;
-        }
-        if(this.head==null){
-        this.head=newNode;
-        }
-        else {
-        INode tempNode=this.head;
-        this.head=newNode;
-        this.head.setNext(tempNode);
-        }
-        }
-public void printMyNodes(){
-        StringBuffer myNodes = new StringBuffer("my Nodes: ");
-        INode tempNode=head;
-        while (tempNode.getNext()!= null){
-        myNodes.append(tempNode.getKey());
-        if (!tempNode.equals(tail))myNodes.append("->");
-        tempNode=tempNode.getNext();
-        }
-        myNodes.append(tempNode.getKey());
-        System.out.println(myNodes);
-        }
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList1 = new LinkedList<>();
+        linkedList1.add(56);
+        linkedList1.add(30);
+        linkedList1.add(70);
+        linkedList1.print();
+        System.out.println();
+    }
 }
